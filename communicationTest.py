ada94e84d38ee5fd079794adee6d9f975dea0336
Gregory Maldonado <gmaldona@oswego.edu>
2020-08-29 16:45:51 -0400

    more testing

--- communicationTest.py
+++ communicationTest.py
@@ -6,20 +6,12 @@ url = 'https://raw.githubusercontent.com/gmaldona/communicationTest/master/commu
 
 response  = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup)
 
-#tree = ET.parse('communication.xml')
-#root = tree.getroot()
-#for message in root.findall('message'):
-# #    message = message.text
-    #print(message)
-
-with open('communication.xml', 'r') as file:
-    reader = file.read()
-    #print(reader)
-    file.close()
-    
 with open('communication.xml', 'w') as file:
-    file.write('<message>')
-    file.write('0')
-    file.write('</message>')
+    file.write(str(soup))
+    
+tree = ET.parse('communication.xml')
+root = tree.getroot()
+for message in root.findall('message'):
+    message = message.text
+    print(message)
