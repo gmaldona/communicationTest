310cf883925e0a0d29f09cec273ff99d544e4b84
Gregory Maldonado <gmaldona@oswego.edu>
2020-08-29 17:00:01 -0400

    change xml

--- communicationTest.py
+++ communicationTest.py
@@ -1,17 +1,27 @@
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 import requests
+   
+def pullXML():
+    url = 'https://raw.githubusercontent.com/gmaldona/communicationTest/master/communication.xml'
 
-url = 'https://raw.githubusercontent.com/gmaldona/communicationTest/master/communication.xml'
-
-response  = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-with open('communication.xml', 'w') as file:
-    file.write(str(soup))
+    response  = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    with open('communication.xml', 'w') as file:
+        file.write(str(soup))
+        
+def readXML():
+    tree = ET.parse('communication.xml')
+    root = tree.getroot()
+    for message in root.findall('message'):
+        message = message.text
+        print(message)
     
-tree = ET.parse('communication.xml')
-root = tree.getroot()
-for message in root.findall('message'):
-    message = message.text
-    print(message)
+def changeXML(m):
+    tree = ET.parse('communication.xml')
+    root = tree.getroot()
+    for message in root.findall('message'):
+        message.text = str(m)
+        tree.write('communication.xml')
+    
+changeXML('1')
